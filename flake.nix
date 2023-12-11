3bf9394ed7faaaf6b7581fae0919e92b432c73c9
redjax <jackenyon@gmail.com>
2023-12-11 17:08:53 +0000

    testing flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
   };
 
   outputs = inputs: {
-    packages."x86_x64-linux".default = derivation {
+    packages."x86_64-linux".default = derivation {
       name = "simple";
-      builder = "${inputs.nixpkgs.legacyPackages."x86_x64-linux".bash}/bin/bash";
+      builder = "${inputs.nixpkgs.legacyPackages."x86_64-linux".bash}/bin/bash";
       args = [ "-c" "echo foo > $out" ];
       src = ./.;
-      system = "x86_x64-linux";
+      system = "x86_64-linux";
     };
   };
 }
