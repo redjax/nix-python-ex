ec5a51947041799f83bb8ce4b92a88c3a8909f81
redjax <jackenyon@gmail.com>
2023-12-11 15:49:12 +0000

    overwrite nix flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,11 @@
 {
-  description = "Example Python package with a NixOS flake.";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  description = "A very basic flake";
 
   outputs = { self, nixpkgs }: {
 
-    defaultPackage.x86_x64-linux =
-    with import nixpkgs { system = "x86_x64-linux"; };
-    stdenv.mkDerivation {
-      name = "hello";
-      src = self;
-      buildPhase = "gcc -o hello ./hello.c";
-      installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
-    };
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
   };
 }
